52c3da3b3a95d7eb884eee505f94597e10273014
leonhard <ljg-hb@web.de>
2016-11-05 19:17:50 +0100

    .

--- src/de/szut/dqi14/vriesgahr/BMI/WeightCategory.java
+++ src/de/szut/dqi14/vriesgahr/BMI/WeightCategory.java
@@ -3,7 +3,7 @@ package de.szut.dqi14.vriesgahr.BMI;
 /** enum class to define weight category
  *
  */
-public abstract enum WeightCategory implements BmiCalc {
+public enum WeightCategory {
 	UNDERWEIGHT, NORMAL, OVERWEIGHT, 
 	OBESE, SEVERELY_OBESE, VERY_SEVERELY_OBESE
 }
